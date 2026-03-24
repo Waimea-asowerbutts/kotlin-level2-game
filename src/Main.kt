022b5c6f7b4d3b68ee5bb23b42b156dbd9a5f444
Waimea-asowerbutts <ajsowerbutts@waimea.school.nz>
2026-03-24 15:07:53 +1300

    Managed to get the code to line up horizontally!
    But you( i ) cannot write anything yet to choose a box or something I'm still working on it

--- src/Main.kt
+++ src/Main.kt
@@ -12,43 +12,42 @@
  */
 
 
-fun printAsHorizTable(squares: List<String>, width: Int = 10) {
-    squares.size
-    val line="─".repeat(width + 2)
-    val top = "╭$line╮"
-    val bot = "╰$line╯"
+fun printAsHorizTable(items: List<Int>, width: Int = 3) {
+    val count = items.size
+    val line = "─".repeat(width + 2)
+    val top = ("╭" + line) + ("┬" + line).repeat(count - 1) + "╮"
+    val bot = ("╰" + line) + ("┴" + line).repeat(count - 1) + "╯"
 
     fun printAsCol(s: String) = print("│ ${s.padStart(width)} ")
 
     println(top)
 
-    for (square in squares)
-        printAsCol("%,d".format(square))
-
+    for (item in items) printAsCol("%,d".format(item))
     println("│")
 
     println(bot)
 }
 
 fun main() {
+    println("Pinned game")
+
+    val distances = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+    printAsHorizTable(distances, 8)
+
 
     val board = (1..16).map { "Square $it" }
     board.chunked(5)
 
-    println("Pinned game")
+
 
     for (i in 1..16) {
 
         val squares = mutableListOf<String>()
 
+        fun printAsHorizTable(items: List<IntRange>) {
 
-        squares.add("Square $i")
-
-        println(squares)
-
-        println("╭────────────╮")
-        println("│            │")
-        println("╰────────────╯")
-
+            val scores = listOf(1..16)
+            printAsHorizTable(scores)
+        }
     }
 }
