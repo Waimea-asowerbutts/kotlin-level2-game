dbcd329572e69015f5436ecbac6b05be5463d7f9
Waimea-asowerbutts <ajsowerbutts@waimea.school.nz>
2026-04-23 12:21:56 +1200

    made turns for each player

--- src/Main.kt
+++ src/Main.kt
@@ -30,73 +30,27 @@ fun printAsHorizTable(items: List<Int>, width: Int = 3) {
 
 fun main() {
 
-    println("Pinned game")
+    println("\uD83D\uDCCCPinned game \uD83D\uDCCC")
+    // Player/User input
+    println("Please Enter Player 1 name here: ")
+    val player1 = readln()
 
-    // Player input
-    println("...you start: ")
+    println("Please Enter Player 2 name here: ")
+    val player2 = readln()
 
-    readln()
+    val players = listOf(player1, player2)
 
-    // attempting to make player turns
-    val players = listOf("Player One", "Player Two")
-    println(players[0])
-    println(players[1])
+    val starter = players.random()
 
-    // har harr har har harrr, har har har har harrrrrr, har har har har, har har har harrrrrrrrrrrrrrr
+    println("\n$starter Goes first!")
+
     println(" Totally Not A RickRoll --> https://www.youtube.com/watch?v=dQw4w9WgXcQ ")
 
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⢳⠃⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠀⠀⠀⠀⠀⡀⠀⠀⠀⠼⢿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠉⠀⠀⠈⠉⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⠿⠟⠛⠋⠛⠻⠿⢿⣿⣿⣿⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠙⠻⢿⣿⣿⡿⠡⠐⠁⠀⠀⠀⠀⠀⠀⠀⡈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠹⠿⠿⠛⠀⠀⠀⠀⠀⢰⢶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⡇⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⡀⠀⠀⠀⠀⣾⣴⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⡇⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠄⠀⡀⠀⠀⣀⡀⢀⣘⣻⣻⡿⣿⣷⣄⣀⣞⣿⡖⠀⠿⣦⠀⠀⠀⠀⠀⠀⠀⠀⣀⠠⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣷⡈⠲⡀⠀⠀⠀⣠⠎⠠⠀⠀⠀⠀⠐⢀⢂⢱⠟⠎⢙⣋⣉⡓⠘⠫⠜⣣⡿⠛⣅⡘⠓⣻⡧⠀⡄⣤⣀⡀⠐⠊⣁⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣶⣦⣤⣤⣾⡏⢠⠶⠿⢿⣿⣷⣦⠀⠀⠀⠠⠾⠛⠻⣿⣿⣷⣆⠀⠉⠁⢀⠸⠇⠈⢧⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⡇⠀⠀⢸⣿⣿⣿⠇⠀⠀⠰⡀⠀⠐⣼⣿⣿⠟⠀⠀⠀⣼⣦⣤⠐⢬⠆⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠙⠲⠾⠿⠛⠋⠁⠀⠀⠀⠀⠙⠛⠛⠛⠋⠁⠀⠀⠀⢀⣿⣿⠇⠘⠞⠣⠄⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠩⠈⠀⠱⢀⡀⢀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠄⠰⣞⠶⣦⣄⣠⣶⣦⠀⢀⣶⣦⣾⣿⣗⡀⢁⠀⢀⣀⠀⠀⠂⠀⠀⠀⣧⠘⣷⣄⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⣄⣺⣥⢛⣶⣿⣿⣿⣿⢠⣿⣿⣿⢿⡛⡿⣿⣿⣷⣾⣟⣉⣻⡷⢀⠀⠀⢸⠀⠽⠙⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⡀⢸⣿⣻⢃⣾⡿⣿⣿⣿⣿⠰⣿⣿⣿⣯⣿⣶⣿⣿⣿⣿⣿⢋⣻⢢⣀⠁⠀⠈⠐⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠉⠀⠻⢛⣿⣿⣿⣧⣾⡀⣘⡿⣿⣿⢻⣆⠻⣟⣿⣿⠷⠋⡉⠀⠁⠀⠀⠀⢠⡄⠀⠀⠀⣀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣤⡄⠀⠁⠈⠀⠀⠀⠈⠀⠀⠀⠁⠈⠀⠀⠀⢀⡀⠀⠀⣄⢀⠀⠀⠀⢈⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠤⠀⠠⠐⠤⠐⠖⠂⠀⣐⠩⠉⠀⠈⠁⠀⠀⠀⠀⢨⠇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣴⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠀⠀⠀⢐⠇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠛⣋⣀⡀⢀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⠀⢦⠇⠀⢀⣀⠞⠛⠀⠈⠙⠛⠛⠛⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⢉⠀⠀⣄⣀⠀⠀⠈⠙⠃⠘⠛⠛⠀⠀⠺⠿⠇⠘⠛⠛⠀⠀⢀⣤⣤⡽⠟⠁⠀⠀⠀⠠⣄⠀⠀⡀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⣿⣿⠋⣠⣔⠛⠂⠀⠻⢻⣗⣴⢤⢀⣀⢤⡀⢠⡀⠀⠀⠀⠀⣀⡀⠀⠐⢲⠚⠛⠀⠁⠀⠀⠀⠀⠀⠀⢺⣷⣼⣿⡿⠆⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⣿⡿⠁⢼⠑⠉⠀⠀⠀⠀⠀⠈⠁⠈⠁⠸⠟⠻⢟⠫⣤⠀⠐⠠⠐⠙⠀⠀⠀⠀⠀⠀⠀⡀⢿⠆⠄⠀⠀⣙⠿⠛⠁⠤⠀⠀⠀⡀⣄⠀⠸⣤⣄⡀⠉⠻⣿⣿⣿⣿⣿")
-    println("⣿⣿⣿⣿⡿⠁⢷⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢰⠀⣄⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⡐⣿⣿⣷⣷⣿⣿⣧⣠⡀⠈⠻⣿⣿⣿")
-    println("⣿⣿⣿⣿⠏⢐⢸⡿⢧⠀⠢⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠀⢠⠋⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠾⣾⣿⢿⣿⣿⣿⣾⢿⣦⡀⠘⢿⣿")
-    println("⣿⣿⠟⠋⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⣤⣦⡌⣳⣿⣄⡀⠀⠀⠀⠂⠠⠠⠚⠁⠿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢹⢟⣻⣽⣿⣿⣿⣿⢽⣿⣤⡈⢿")
-    println("⠏⣡⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢄⢰⣟⡣⠶⠗⠃⣰⢚⠋⠿⢿⣿⣿⠷⠦⠤⡠⢤⠀⠴⢶⣤⣴⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠉⠵⠝⡳⠶⠌⢻⡻⠶⠧⠈")
-
-// Randomised counters between both players (limit of players is two)
-    fun counters() {
-    println("White Counter")
-    println("Black Counter")
-
-    }
+    //working on scoring system. . .
 
     val distances = (1..16).toList()
 
     printAsHorizTable(distances, 8)
-
-    //Random Black Squares, every time the user plays the game it will be randomly generated from 1..16
-    val nums = listOf(1..16)
-
-    nums.size - 1..16
-
-    nums[0]
-    nums.first()
-    nums.last()
-
-    val sums = listOf(1..16)
-
-    sums[0]
-
-    // Working on a limiter, making it say you lost or will count towards being incorrect for the counters.
 
     val board = (1..16).map { "Square $it" }
     board.chunked(5)
@@ -107,33 +61,7 @@ fun main() {
 
         fun printAsHorizTable(items: List<IntRange>) {
 
-            val scores = listOf(1..16)
-            printAsHorizTable(scores)
+
         }
     }
 }
-
-//⠀⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⢰⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠈⢿⣿⣿⣿⣗⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠘⣿⣿⣿⣿⣷⣬⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠈⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⣿⣿⣿⣿⡃⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⢻⣿⣯⣅⣰⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⢀⠤⠶⢶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⠀⣿⣿⣿⣿⣿⡏⠀⠀⠀⣼⣧⡀⠀⠀⠈⠛⣆⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⠀⠀⣰⣿⣟⠿⣗⠾⢺⣦⡸⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⣿⣿⣿⣿⣿⣯⣿⡄⠀⢹⣿⣿⠳⡄⣿⣤⠉⡁⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⡇⠀⠀⢹⣿⣡⡻⠿⢟⢠⠁⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⡧⡀⢠⣾⣿⣿⣿⣶⣶⠇⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⢇⠁⠰⣄⡀⠙⢸⣽⢁⠀⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⠀⣿⣿⣿⣿⣿⡿⡼⣁⣀⣜⣶⣤⡀⠑⠄⠠⠀⠀⠀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⠀⠘⣿⣿⣿⣻⡟⠟⣿⣇⠀⠉⡉⢷⢄⡸⠀⠐⠀⡀⠀⠀⠀⠀⠀//
-//⠀⠀⠀⠀⠀⢻⣿⣿⣼⢠⢠⣿⣿⡇⠀⠀⠘⣶⠧⣫⠉⢀⠀⠠⠀⠀⠀⠀//
-//⠀⠀⠀⠀⠀⢸⣿⣿⣿⣯⠚⣿⣿⣗⠒⠀⠀⠀⢏⣮⡅⠘⠳⣄⠳⠀⠀⠀//
-//⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⡷⠆⠀⡀⠈⠹⣶⣤⠀⢉⠄⢂⠀⠀//
-//⠀⠀⠀⠀⠀⠀⢻⣻⣿⣿⣿⣿⣿⣿⣿⣶⣦⡇⣥⡢⠸⣷⢦⣘⠓⢪⡀⠀//
-//⠀⠀⠀⠀⠀⢀⡜⠩⠭⠉⠉⠉⠙⠛⠿⠿⣿⣹⣿⡿⠇⠀⠋⠛⠆⡿⢃⠀//
-//⠀⠀⠀⠀⢀⢯⣄⡀⠀⢀⢴⠀⠠⠀⠀⠀⢈⣨⣤⢄⣀⠀⠀⣆⠀⠑⢬⡆//
-//⠀⠀⠀⢀⠂⠀⠟⠋⠍⠋⡌⠀⠀⠀⠀⠀⡌⠈⢿⣯⣷⡀⣀⣿⣀⠀⠀⠈//
-//Auf der Heide blüht ein kleines Blümelein) Auf der Heide blüht ein kleines Blümelein Und das heißt: Erika. Heiß von hunderttausend kleinen Bienelein Wird umschwärmt: Erika. Denn ihr Herz ist voller Süßigkeit, Zarter Duft entströmt dem Blütenkleid. Auf der Heide blüht ein kleines Blümelein Und das heißt: Erika.
